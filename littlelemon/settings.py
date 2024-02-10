1e75723be9c37fd8d9825b77bc3683df507030c0
Sephora Bemba <bemba.sephora@gmail.com>
2024-02-11 00:05:16 +0100

    ENH: Add restaurant images and static route for restaurant app

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    "restaurant/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
