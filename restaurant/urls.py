0141ec17f2c866b64ebbe576056349a5fc93a3ad
Sephora Bemba <bemba.sephora@gmail.com>
2024-02-13 15:05:58 +0100

    MAINT: replace menu html by browsable api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,7 +6,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("auth/api-token-auth", obtain_auth_token),
     path("api/menu/items", views.MenuItemViewAPI.as_view(), name="menu-items-api"),
-    path("restaurant/menu/items", login_required(views.MenuItemViewHTML.as_view()), name="menu-items"),
+    #path("restaurant/menu/items", login_required(views.MenuItemViewHTML.as_view()), name="menu-items"),
+    path("restaurant/menu/items", views.MenuItemViewAPI.as_view(), name="menu-items"),
     path("api/menu/items/<int:pk>", views.SingleMenuItemViewAPI.as_view(), name="menu-item-api"),
-    path("restaurant/menu/items/<int:pk>", login_required(views.SingleMenuItemViewHTML.as_view()), name="menu-item"),
+    #path("restaurant/menu/items/<int:pk>", login_required(views.SingleMenuItemViewHTML.as_view()), name="menu-item"),
+    path("restaurant/menu/items/<int:pk>", views.SingleMenuItemViewAPI.as_view(), name="menu-item"),
 ]
