2519f89cfbc46ecd19b69682b787235734514244
Sephora Bemba <bemba.sephora@gmail.com>
2024-02-13 14:07:12 +0100

    TEST: Add Booking tests

--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -1,14 +1,59 @@
 from django.test import TestCase
 from restaurant.models import Booking, Menu
+import datetime
+from decimal import Decimal
 
 class MenuTest(TestCase):
     def setUp(self):
         pass
+    
     def tearDown(self):
-        pass
+        Menu.objects.all().delete()
     
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        item_str = str(item)
+        output = str(item)
         expected = "IceCream : 80"
-        self.assertEqual(item_str, expected)
+        self.assertEqual(output, expected)
+        
+        
+    def test_update_item(self):
+        item = Menu.objects.create(title="Pancake", price=12.11, inventory=100)
+        item = Menu.objects.get(title="Pancake")
+        item.title = "Pancake Updated"
+        item.price = Decimal("19.11")
+        item.inventory = 10
+        item.save()
+        
+        item_refreshed = Menu.objects.get(title="Pancake Updated")
+        self.assertEqual(item.title, item_refreshed.title)
+        self.assertEqual(item.price, item_refreshed.price)
+        self.assertEqual(item.inventory, item_refreshed.inventory)
+
+class BookingTest(TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        Booking.objects.all().delete()
+    
+    def test_get_item(self):
+        booking_date = datetime.datetime.strptime("2026-02-13 06:18:00", "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=datetime.timezone.utc)
+        item = Booking.objects.create(name="John Land", no_of_guests=12, booking_date=booking_date)
+        output = str(item)
+        expected = "John Land - 2026-02-13 06:18:00"
+        self.assertEqual(output, expected)
+
+    def test_update_item(self):
+        booking_date = datetime.datetime.strptime("2026-02-15 02:38:00", "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=datetime.timezone.utc)
+        item = Booking.objects.create(name="Stan Paul", no_of_guests=2, booking_date=booking_date)
+        item = Booking.objects.get(name="Stan Paul")
+        item.name = "Stan Pauldeene"
+        item.no_of_guests = 12
+        item.booking_date = datetime.datetime.strptime("2026-02-18 18:00:00", "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=datetime.timezone.utc)
+        item.save()
+        
+        item_refreshed = Booking.objects.get(name="Stan Pauldeene")
+        self.assertEqual(item.name, item_refreshed.name)
+        self.assertEqual(item.no_of_guests, item_refreshed.no_of_guests)
+        self.assertEqual(item.booking_date, item_refreshed.booking_date)
--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 from django.test import TestCase
 from restaurant.models import Menu, Booking
 from restaurant.serializers import MenuSerializer, BookingSerializer
+import datetime
 
 class MenuViewTest(TestCase):
     def setUp(self):
@@ -11,7 +12,7 @@ class MenuViewTest(TestCase):
         self.item3 = Menu.objects.create(title="Carrot soup", price=110.98, inventory=1)
         
     def tearDown(self):
-        pass
+        Menu.objects.all().delete()
     
     def test_get_all(self):
         # retrieve all the Menu objects added for the test purpose.
@@ -19,12 +20,48 @@ class MenuViewTest(TestCase):
 
         # serialize the retrieved objects
         serialized_data = MenuSerializer(items, many=True).data
-        
+        for record in serialized_data:
+            del record["id"]
+
         # Define the expected queryset of Menu Items
         expected_queryset = [
-            OrderedDict([('id', 2), ('title', 'IceCream'), ('price', "80.00"), ('inventory', 100)]),
-            OrderedDict([('id', 3), ('title', 'Chocolate cake'), ('price', "110.98"), ('inventory', 12)]),
-            OrderedDict([('id', 4), ('title', 'Carrot soup'), ('price', "110.98"), ('inventory', 1)])
+            OrderedDict([('title', 'IceCream'), ('price', "80.00"), ('inventory', 100)]),
+            OrderedDict([ ('title', 'Chocolate cake'), ('price', "110.98"), ('inventory', 12)]),
+            OrderedDict([ ('title', 'Carrot soup'), ('price', "110.98"), ('inventory', 1)])
+        ]
+        
+        # Check if the serialized data matches the expected queryset
+        self.assertEqual(serialized_data, expected_queryset)
+
+class BookingViewTest(TestCase):
+    def setUp(self):
+        # Use the setup() method to add a few test instances
+        self.booking_date1 = datetime.datetime.strptime("2026-02-13 06:18:00", "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=datetime.timezone.utc)
+        self.item1 = Booking.objects.create(name="Lana Mac", no_of_guests=3, booking_date=self.booking_date1)
+        
+        self.booking_date2 = datetime.datetime.strptime("2026-09-22 16:30:00", "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=datetime.timezone.utc)
+        self.item2 = Booking.objects.create(name="Peter7Y677YHUIHJK@jkj", no_of_guests=31, booking_date=self.booking_date2)
+        
+        self.booking_date3 = datetime.datetime.strptime("2029-11-02 12:15:00", "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=datetime.timezone.utc)
+        self.item3= Booking.objects.create(name="7897897_Bennington", no_of_guests=11, booking_date=self.booking_date3)
+        
+    def tearDown(self):
+        Booking.objects.all().delete()
+    
+    def test_get_all(self):
+        # retrieve all the objects added for the test purpose.
+        items = Booking.objects.all()
+
+        # serialize the retrieved objects
+        serialized_data = BookingSerializer(items, many=True).data
+        for record in serialized_data:
+            del record["id"]
+
+        # Define the expected queryset
+        expected_queryset = [
+            OrderedDict([('name', "Lana Mac"), ('no_of_guests', 3), ('booking_date', '2026-02-13T06:18:00Z')]),
+            OrderedDict([ ('name', "Peter7Y677YHUIHJK@jkj"), ('no_of_guests', 31), ('booking_date', '2026-09-22T16:30:00Z')]),
+            OrderedDict([ ('name', "7897897_Bennington"), ('no_of_guests', 11), ('booking_date', '2029-11-02T12:15:00Z')])
         ]
         
         # Check if the serialized data matches the expected queryset
